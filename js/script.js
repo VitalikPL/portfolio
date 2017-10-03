497d4909ff9186be1a19025cc6616a04dfdbff13
VitalikPL <vitalik_zip@bigmir.net>
2017-10-04 00:59:48 +0300

    newcomit

--- js/script.js
+++ js/script.js
@@ -5,4 +5,22 @@ function myFunction () {
 	} else {
 		x.className = "topnav";
 	}
+};
+// Scrol
+window.onscroll = function () {
+	var scrollElem = document.getElementById('scrollTop');
+	if(document.body.scrollTop < document.documentElement.clientHeight) {
+		scrollElem.style.opacity = "1";
+	} else {
+		scrollElem.style.opacity = "0";
+	}
+};
+var timeOut;
+function goUP() {
+	var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+	if (top>0) {
+		window.scrollBy(0, -100);
+		timeOut = setTimeout('goUP()', 20);
+	} else clearTimeout(timeOut);
 }
+
